877e5f795304b8369479c2db378351b58e42040d
Ankesh Anil Gaikwad <42441317+ankesh06g@users.noreply.github.com>
2018-10-31 07:52:46 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<meta charset="utf-8">
+<html>
+    <head>
+        <title>Ankesh</title> 
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <div class="header">
+            <div class="logo">Ankesh Gaikwad</div>
+        </div>
+        <div class="sidemenu">
+            <a href="display.php" target="window"><div class="tab">Display</div></a>
+            <a href="add.php" target="window"><div class="tab">Add</div></a>
+            <a href="delete.php" target="window"><div class="tab">Delete</div></a>
+        </div>
+        <iframe name="window" src="display.php"></iframe>
+        <div class="footer">
+            All copyrights reserved By @nkesh
+        </div>
+    </body>
+</html>
